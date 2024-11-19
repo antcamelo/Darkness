2b75f35e099accd391b44442ec692d5c26c09868
Your Name <brunobrmayer@gmail.com>
2024-11-19 12:58:36 -0500

    Arrumando o mapa

--- src/main.c
+++ src/main.c
@@ -64,61 +64,62 @@ int main() {
         obst[i][3] = 1; 
     }
 
-    obst[78][6] = 1; 
-    obst[78][15] = 1;
+    obst[78][15] = 1; //invertido
+    obst[78][6] = 1;
 
 
-    obst[77][6] = 1; 
-    obst[77][15] = 1;
+    obst[77][15] = 1; //invertido
+    obst[77][6] = 1;
 
-    for (int row = 6; row <= 12; row++) {
+    for (int row = 12; row <= 6; row++) {
         obst[76][row] = 1;
-        if (row == 12) {
+        if (row == 12) {//invertido
             break;
         }
     }
     
-    obst[76][15] = 1;
-    obst[76][18] = 1; 
     obst[76][21] = 1;
+    obst[76][18] = 1; //invertido
+    obst[76][15] = 1;
 
-    obst[75][12] = 1; 
-    obst[75][15] = 1;
-    obst[75][18] = 1; 
-    obst[75][21] = 1;
+    obst[75][21] = 1; 
+    obst[75][18] = 1;
+    obst[75][15] = 1; //invertido
+    obst[75][12] = 1;
 
-    obst[74][12] = 1; 
-    obst[74][15] = 1;
-    obst[74][18] = 1; 
-    obst[74][21] = 1;
+    obst[74][21] = 1; 
+    obst[74][18] = 1;//invertido
+    obst[74][15] = 1; 
+    obst[74][12] = 1;
 
-    obst[73][4] = 1; 
-    obst[73][5] = 1;
-    obst[73][6] = 1; 
-    obst[73][9] = 1;
-    obst[73][10] = 1; 
-    obst[73][11] = 1;
-    obst[73][12] = 1; 
-    obst[73][15] = 1;
-    obst[73][18] = 1; 
-    obst[73][21] = 1;
-    obst[73][22] = 1; 
-    obst[73][23] = 1;
+    obst[73][23] = 1; 
+    obst[73][22] = 1;
+    obst[73][21] = 1; 
+    obst[73][18] = 1;
+    obst[73][15] = 1; 
+    obst[73][12] = 1;
+    obst[73][11] = 1; 
+    obst[73][10] = 1;
+    obst[73][9] = 1; //invertido
+    obst[73][6] = 1;
+    obst[73][5] = 1; 
+    obst[73][4] = 1;
 
-    obst[72][9] = 1; 
-    obst[72][15] = 1;
-    obst[72][18] = 1; 
-    obst[72][21] = 1;
+    obst[72][21] = 1; 
+    obst[72][18] = 1;
+    obst[72][15] = 1; //invertido
+    obst[72][9] = 1;
 
-    obst[71][9] = 1; 
-    obst[71][15] = 1;
-    obst[71][18] = 1; 
-    obst[71][21] = 1;
+    obst[71][21] = 1; 
+    obst[71][18] = 1;
+    obst[71][15] = 1; //invertido
+    obst[71][9] = 1;
 
-    obst[70][6] = 1; 
-    obst[70][9] = 1;
-    for (int row = 12; row <= 18; row++) {
-        obst[70][row] = 1;
+    obst[70][9] = 1; 
+    obst[70][6] = 1;//invertido
+
+    for (int row = 18; row <= 12; row++) {
+        obst[70][row] = 1;//invertido
         if (row == 18) {
             break;
         }
@@ -128,7 +129,8 @@ int main() {
     obst[69][6] = 1;
 
     obst[68][6] = 1;
-    for (int row = 6; row <= 23; row++) {
+
+    for (int row = 23; row <= 6; row++) {
         obst[67][row] = 1;
         if (row == 23) {
             break;
@@ -142,71 +144,68 @@ int main() {
     obst[65][6] = 1;
 
 
-    obst[64][6] = 1;
-    obst[64][7] = 1; 
-    obst[64][8] = 1;
-    obst[64][9] = 1; 
-    obst[64][10] = 1;
-    obst[64][11] = 1; 
-    obst[64][12] = 1;
-    obst[64][15] = 1; 
-    obst[64][16] = 1;
-    obst[64][17] = 1; 
-    obst[64][18] = 1;
-    obst[64][19] = 1; 
-    obst[64][20] = 1;
-    obst[64][21] = 1; 
-    obst[64][22] = 1;
-    obst[64][23] = 1; 
+    obst[64][23] = 1;
+    obst[64][22] = 1; 
+    obst[64][21] = 1;
+    obst[64][20] = 1; 
+    obst[64][19] = 1;
+    obst[64][18] = 1; 
+    obst[64][17] = 1;
+    obst[64][16] = 1; 
+    obst[64][15] = 1;
+    obst[64][12] = 1; 
+    obst[64][11] = 1;
+    obst[64][10] = 1; //invertido
+    obst[64][9] = 1;
+    obst[64][8] = 1; 
+    obst[64][7] = 1;
+    obst[64][6] = 1; 
 
 
-    obst[63][6] = 1;
-    obst[63][12] = 1; 
-    obst[63][15] = 1;
-    obst[63][21] = 1; 
+    obst[63][21] = 1;
+    obst[63][15] = 1; 
+    obst[63][12] = 1;//invertido
+    obst[63][6] = 1; 
 
 
-    obst[62][6] = 1;
-    obst[62][12] = 1; 
-    obst[62][15] = 1;
     obst[62][21] = 1;
+    obst[62][15] = 1; //invertido
+    obst[62][12] = 1;
+    obst[62][6] = 1;
 
 
-    obst[61][6] = 1;
-    obst[61][9] = 1; 
-    obst[61][12] = 1;
-    obst[61][15] = 1;
-    obst[61][6] = 1;
-    obst[61][12] = 1; 
-    obst[61][15] = 1;
-    obst[61][18] = 1;
-    obst[61][19] = 1;
-    obst[61][20] = 1; 
     obst[61][21] = 1;
+    obst[61][20] = 1; 
+    obst[61][19] = 1;
+    obst[61][18] = 1;
+    obst[61][15] = 1;
+    obst[61][12] = 1;//invertido
+    obst[61][9] = 1; 
+    obst[61][6] = 1;
 
 
-    obst[60][9] = 1;
-    obst[60][12] = 1; 
     obst[60][15] = 1;
+    obst[60][12] = 1; //invertido
+    obst[60][9] = 1;
 
 
-    obst[59][9] = 1;
-    obst[59][12] = 1; 
     obst[59][15] = 1;
+    obst[59][12] = 1; //invertido
+    obst[59][9] = 1;
 
 
-    obst[58][4] = 1;
-    obst[58][5] = 1; 
-    obst[58][6] = 1;
-    obst[58][9] = 1;
-    obst[58][10] = 1; 
-    obst[58][11] = 1;
-    obst[58][12] = 1;
-    obst[58][15] = 1;
-    obst[58][18] = 1;
-    obst[58][21] = 1;
-    obst[58][22] = 1; 
     obst[58][23] = 1;
+    obst[58][22] = 1; 
+    obst[58][21] = 1;
+    obst[58][18] = 1;
+    obst[58][15] = 1; 
+    obst[58][12] = 1;
+    obst[58][11] = 1;
+    obst[58][10] = 1;
+    obst[58][9] = 1;
+    obst[58][6] = 1;//invertido
+    obst[58][5] = 1; 
+    obst[58][4] = 1;
 
 
     obst[57][18] = 1;
@@ -215,44 +214,43 @@ int main() {
     obst[56][18] = 1;
 
 
-    obst[55][4] = 1;
-    obst[55][5] = 1; 
-    obst[55][6] = 1;
-    obst[55][7] = 1;
-    obst[55][8] = 1; 
-    obst[55][9] = 1;
-    obst[55][10] = 1;
-    obst[55][11] = 1;
-    obst[55][12] = 1;
-    obst[55][13] = 1;
-    obst[55][14] = 1; 
-    obst[55][15] = 1;
-    obst[55][18] = 1;
-    obst[55][19] = 1; 
-    obst[55][20] = 1;
-    obst[55][21] = 1;
-    obst[55][22] = 1; 
     obst[55][23] = 1;
+    obst[55][22] = 1; 
+    obst[55][21] = 1;
+    obst[55][20] = 1;
+    obst[55][19] = 1; 
+    obst[55][18] = 1;
+    obst[55][15] = 1;
+    obst[55][14] = 1;
+    obst[55][13] = 1;
+    obst[55][12] = 1;//invertido
+    obst[55][11] = 1; 
+    obst[55][10] = 1;
+    obst[55][9] = 1;
+    obst[55][8] = 1; 
+    obst[55][7] = 1;
+    obst[55][6] = 1;
+    obst[55][5] = 1; 
+    obst[55][4] = 1;
 
 
-    obst[52][4] = 1;
-    obst[52][5] = 1; 
-    obst[52][6] = 1;
-    obst[52][9] = 1;
-    obst[52][10] = 1; 
-    obst[52][11] = 1;
-    obst[52][12] = 1;
-    obst[52][13] = 1; 
-    obst[52][14] = 1;
-    obst[52][15] = 1;
-    obst[52][16] = 1; 
-    obst[52][17] = 1;
     obst[52][18] = 1;
-    obst[52][19] = 1; 
-    obst[52][20] = 1;
-    obst[52][21] = 1;
-    obst[52][22] = 1; 
+    obst[52][17] = 1; 
+    obst[52][16] = 1;
+    obst[52][15] = 1;
+    obst[52][14] = 1; 
+    obst[52][13] = 1;
+    obst[52][12] = 1;
+    obst[52][11] = 1; //invertido
+    obst[52][10] = 1;
+    obst[52][9] = 1;
+    obst[52][6] = 1; 
+    obst[52][5] = 1;
+    obst[52][4] = 1;
+ 
     obst[52][23] = 1;
+    obst[52][22] = 1; //invertido
+    obst[52][21] = 1;
 
 
     obst[51][9] = 1;
@@ -261,92 +259,92 @@ int main() {
     obst[50][9] = 1;
 
 
-    obst[49][4] = 1;
-    obst[49][5] = 1;
-    obst[49][6] = 1;
-    obst[49][9] = 1;
-    obst[49][12] = 1;
-    obst[49][13] = 1;
-    obst[49][14] = 1;
-    obst[49][15] = 1;
-    obst[49][16] = 1;
-    obst[49][17] = 1;
-    obst[49][18] = 1;
-    obst[49][21] = 1;
-    obst[49][22] = 1;
     obst[49][23] = 1;
+    obst[49][22] = 1;
+    obst[49][21] = 1;
+    obst[49][18] = 1;
+    obst[49][17] = 1;
+    obst[49][16] = 1;
+    obst[49][15] = 1;//invertido
+    obst[49][14] = 1;
+    obst[49][13] = 1;
+    obst[49][12] = 1;
+    obst[49][9] = 1;
+    obst[49][6] = 1;
+    obst[49][5] = 1;
+    obst[49][4] = 1;
 
 
-    obst[48][6] = 1;
     obst[48][12] = 1;
+    obst[48][6] = 1;//invertido
 
 
+    obst[47][12] = 1;//invertido
     obst[47][6] = 1;
-    obst[47][12] = 1;
 
 
-    obst[46][6] = 1;
-    obst[46][9] = 1;
-    obst[46][10] = 1;
-    obst[46][11] = 1;
-    obst[46][12] = 1;
-    obst[46][13] = 1;
-    obst[46][14] = 1;
-    obst[46][15] = 1;
-    obst[46][18] = 1;
     obst[46][21] = 1;
+    obst[46][18] = 1;
+    obst[46][15] = 1;//invertido
+    obst[46][14] = 1;
+    obst[46][13] = 1;
+    obst[46][12] = 1;
+    obst[46][11] = 1;
+    obst[46][10] = 1;
+    obst[46][9] = 1;
+    obst[46][6] = 1;
 
 
-    obst[45][6] = 1;
-    obst[45][9] = 1;
-    obst[45][18] = 1;
     obst[45][21] = 1;
+    obst[45][18] = 1;//invertido
+    obst[45][9] = 1;
+    obst[45][6] = 1;
 
 
-    obst[44][6] = 1;
-    obst[44][9] = 1;
-    obst[44][18] = 1;
     obst[44][21] = 1;
+    obst[44][18] = 1;
+    obst[44][9] = 1;//invertido
+    obst[44][6] = 1;
 
 
-    obst[43][6] = 1;
-    obst[43][7] = 1;
-    obst[43][8] = 1;
-    obst[43][9] = 1;
-    obst[43][12] = 1;
-    obst[43][13] = 1;
-    obst[43][14] = 1;
-    obst[43][15] = 1;
-    obst[43][18] = 1;
-    obst[43][19] = 1;
-    obst[43][20] = 1;
     obst[43][21] = 1;
+    obst[43][20] = 1;
+    obst[43][19] = 1;
+    obst[43][18] = 1;
+    obst[43][15] = 1;
+    obst[43][14] = 1;
+    obst[43][13] = 1;//invertido
+    obst[43][12] = 1;
+    obst[43][9] = 1;
+    obst[43][8] = 1;
+    obst[43][7] = 1;
+    obst[43][26] = 1;
 
 
-    obst[42][12] = 1;
     obst[42][21] = 1;
+    obst[42][12] = 1;//invertido
 
 
+    obst[41][21] = 1;//invertido
     obst[41][12] = 1;
-    obst[41][21] = 1;
 
 
-    obst[40][6] = 1;
-    obst[40][7] = 1;
-    obst[40][8] = 1;
-    obst[40][9] = 1;
-    obst[40][10] = 1;
-    obst[40][11] = 1;
-    obst[40][12] = 1;
-    obst[40][13] = 1;
-    obst[40][14] = 1;
-    obst[40][15] = 1;
-    obst[40][16] = 1;
-    obst[40][17] = 1;
-    obst[40][18] = 1;
-    obst[40][21] = 1;
-    obst[40][22] = 1;
     obst[40][23] = 1;
+    obst[40][22] = 1;
+    obst[40][21] = 1;
+    obst[40][18] = 1;
+    obst[40][17] = 1;
+    obst[40][16] = 1;
+    obst[40][15] = 1;
+    obst[40][14] = 1;
+    obst[40][13] = 1;
+    obst[40][12] = 1;
+    obst[40][11] = 1;
+    obst[40][10] = 1;//invertido
+    obst[40][9] = 1;
+    obst[40][8] = 1;
+    obst[40][7] = 1;
+    obst[40][6] = 1;
 
 
     obst[39][9] = 1;
@@ -355,162 +353,166 @@ int main() {
     obst[38][9] = 1;
 
 
-    obst[37][4] = 1;
-    obst[37][5] = 1;
-    obst[37][6] = 1;
-    obst[37][9] = 1;
-    obst[37][12] = 1;
-    obst[37][15] = 1;
-    obst[37][16] = 1;
-    obst[37][17] = 1;
-    obst[37][18] = 1;
-    obst[37][19] = 1;
-    obst[37][20] = 1;
-    obst[37][21] = 1;
-    obst[37][22] = 1;
     obst[37][23] = 1;
+    obst[37][22] = 1;
+    obst[37][21] = 1;//invertido
+    obst[37][20] = 1;
+    obst[37][19] = 1;
+    obst[37][18] = 1;
+    obst[37][17] = 1;
+    obst[37][16] = 1;
+    obst[37][15] = 1;
+    obst[37][12] = 1;
+    obst[37][9] = 1;
+    obst[37][6] = 1;
+    obst[37][5] = 1;
+    obst[37][4] = 1;
 
 
-    obst[36][6] = 1;
-    obst[36][12] = 1;
-    obst[36][15] = 1;
     obst[36][18] = 1;
+    obst[36][15] = 1;
+    obst[36][12] = 1;
+    obst[36][6] = 1;//invertido
 
 
-    obst[35][6] = 1;
-    obst[35][12] = 1;
-    obst[35][15] = 1;
     obst[35][18] = 1;
+    obst[35][15] = 1;
+    obst[35][12] = 1;
+    obst[35][6] = 1;//invertido
 
 
-    obst[34][6] = 1;
-    obst[34][12] = 1;
-    obst[34][15] = 1;
-    obst[34][18] = 1;
-    obst[34][19] = 1;
-    obst[34][20] = 1;
     obst[34][21] = 1;
+    obst[34][20] = 1;
+    obst[34][19] = 1;
+    obst[34][18] = 1;//invertido
+    obst[34][15] = 1;
+    obst[34][12] = 1;
+    obst[34][6] = 1;
 
 
-    obst[33][9] = 1;
     obst[33][12] = 1;
+    obst[33][9] = 1;//invertido
 
 
-    obst[32][9] = 1;
     obst[32][12] = 1;
+    obst[32][9] = 1;//invertido
 
 
-    for (int row = 4; row <= 9; row++) {
-        obst[31][row] = 1;
+    for (int row = 9; row <= 4; row++) {
+        obst[31][row] = 1;//invertido
         if (row == 9) {
             break;
         }
     }
 
-    obst[31][12] = 1;
-    obst[31][13] = 1;
-    obst[31][14] = 1;
     obst[31][15] = 1;
-    for (int row = 18; row <= 23; row++) {
-        obst[31][row] = 1;
+    obst[31][14] = 1;
+    obst[31][13] = 1;//invertido
+    obst[31][12] = 1;
+
+    for (int row = 23; row <= 18; row++) {
+        obst[31][row] = 1;//invertido
         if (row == 23) {
             break;
         }
     }
 
 
-    obst[28][6] = 1;
-    obst[28][7] = 1;
-    obst[28][8] = 1;
     obst[28][9] = 1;
-    for (int row = 12; row <= 21; row++) {
-        obst[28][row] = 1;
+    obst[28][8] = 1;//invertido
+    obst[28][7] = 1;
+    obst[28][6] = 1;
+
+    for (int row = 21; row <= 12; row++) {
+        obst[28][row] = 1;//invertido
         if (row == 21) {
             break;
         }
     }
 
 
-    obst[27][15] = 1;
     obst[27][21] = 1;
+    obst[27][15] = 1;//invertido
 
 
+    obst[26][21] = 1;//invertido
     obst[26][15] = 1;
-    obst[26][21] = 1;
 
 
-    obst[25][4] = 1;
-    obst[25][5] = 1;
-    obst[25][6] = 1;
-    obst[25][9] = 1;
-    obst[25][12] = 1;
-    obst[25][13] = 1;
-    obst[25][14] = 1;
-    obst[25][15] = 1;
-    obst[25][18] = 1;
     obst[25][21] = 1;
+    obst[25][18] = 1;
+    obst[25][15] = 1;
+    obst[25][14] = 1;
+    obst[25][13] = 1;
+    obst[25][12] = 1;
+    obst[25][9] = 1;
+    obst[25][6] = 1;//invertido
+    obst[25][5] = 1;
+    obst[25][4] = 1;
 
 
-    obst[24][6] = 1;
-    obst[24][9] = 1;
-    obst[24][12] = 1;
     obst[24][18] = 1;
+    obst[24][12] = 1;
+    obst[24][9] = 1;//invertido
+    obst[24][6] = 1;
 
 
-    obst[23][6] = 1;
-    obst[23][9] = 1;
-    obst[23][12] = 1;
     obst[23][18] = 1;
+    obst[23][12] = 1;
+    obst[23][9] = 1;
+    obst[23][6] = 1;//invertido
 
 
-    obst[22][6] = 1;
-    obst[22][9] = 1;
-    obst[22][10] = 1;
-    obst[22][11] = 1;
     obst[22][12] = 1;
-    for (int row = 15; row <= 23; row++) {
+    obst[22][11] = 1;
+    obst[22][10] = 1;
+    obst[22][9] = 1;//invertido
+    obst[22][6] = 1;
+
+    for (int row = 23; row <= 15; row++) {
         obst[22][row] = 1;
-        if (row == 23) {
+        if (row == 23) {//invertido
             break;
         }
     }
 
 
-    obst[21][6] = 1;
     obst[21][15] = 1;
+    obst[21][6] = 1;//invertido
 
 
-    obst[20][6] = 1;
     obst[20][15] = 1;
+    obst[20][6] = 1;//invertido
 
 
-    for (int row = 6; row <= 12; row++) {
-        obst[19][row] = 1;
+    for (int row = 12; row <= 6; row++) {
+        obst[19][row] = 1;                  //invertido
         if (row == 12) {
             break;
         }
     }
-    obst[19][15] = 1;
-    obst[19][18] = 1;
-    obst[19][21] = 1;
-    obst[19][22] = 1;
     obst[19][23] = 1;
+    obst[19][22] = 1;
+    obst[19][21] = 1;
+    obst[19][18] = 1;//invertido
+    obst[19][15] = 1;
 
 
-    obst[18][15] = 1;
     obst[18][18] = 1;
+    obst[18][15] = 1;//invertido
 
 
-    obst[17][15] = 1;
     obst[17][18] = 1;
+    obst[17][15] = 1;//invertido
 
 
-    obst[16][6] = 1;
-    obst[16][7] = 1;
-    obst[16][8] = 1;
-    obst[16][9] = 1;
     obst[16][12] = 1;
-    for (int row = 15; row <= 21; row++) {
+    obst[16][9] = 1;
+    obst[16][8] = 1;//invertido
+    obst[16][7] = 1;
+    obst[16][6] = 1;
+
+    for (int row = 21; row <= 15; row++) {
         obst[16][row] = 1;
         if (row == 21) {
             break;
@@ -518,97 +520,98 @@ int main() {
     }
 
 
-    obst[15][9] = 1;
-    obst[15][12] = 1;
     obst[15][18] = 1;
+    obst[15][12] = 1;//invertido
+    obst[15][9] = 1;
 
 
-    obst[14][9] = 1;
-    obst[14][12] = 1;
     obst[14][18] = 1;
+    obst[14][12] = 1;//invertido
+    obst[14][9] = 1;
 
 
-    for (int row = 4; row <= 15; row++) {
+    for (int row = 15; row <= 4; row++) {
         obst[13][row] = 1;
         if (row == 15) {
             break;
         }
     }
-    obst[13][18] = 1;
-    obst[13][19] = 1;
-    obst[13][20] = 1;
-    obst[13][21] = 1;
-    obst[13][22] = 1;
     obst[13][23] = 1;
+    obst[13][22] = 1;
+    obst[13][21] = 1;  // invertido
+    obst[13][20] = 1;
+    obst[13][19] = 1;
+    obst[13][18] = 1;
 
 
-    obst[12][9] = 1;
     obst[12][15] = 1;
+    obst[12][9] = 1; //invertido
 
 
-    obst[11][9] = 1;
     obst[11][15] = 1;
+    obst[11][9] = 1;//invertido
 
 
-    obst[10][6] = 1;
-    obst[10][9] = 1;
-    obst[10][12] = 1;
-    obst[10][15] = 1;
-    obst[10][16] = 1;
-    obst[10][17] = 1;
-    obst[10][18] = 1;
     obst[10][21] = 1;
+    obst[10][18] = 1;
+    obst[10][17] = 1;
+    obst[10][16] = 1;
+    obst[10][15] = 1;//invertido
+    obst[10][12] = 1;
+    obst[10][9] = 1;
+    obst[10][6] = 1;
 
 
-    obst[9][6] = 1;
-    obst[9][9] = 1;
-    obst[9][12] = 1;
-    obst[9][15] = 1;
-    obst[9][18] = 1;
     obst[9][21] = 1;
+    obst[9][18] = 1;
+    obst[9][15] = 1;//invertido
+    obst[9][12] = 1;
+    obst[9][9] = 1;
+    obst[9][6] = 1;
 
 
-    obst[8][6] = 1;
-    obst[8][9] = 1;
-    obst[8][12] = 1;
-    obst[8][15] = 1;
-    obst[8][18] = 1;
     obst[8][21] = 1;
+    obst[8][18] = 1;
+    obst[8][15] = 1;
+    obst[8][12] = 1;//invertido
+    obst[8][9] = 1;
+    obst[8][6] = 1;
 
 
-    obst[7][4] = 1;
-    obst[7][5] = 1;
-    obst[7][6] = 1;
-    obst[7][9] = 1;
-    obst[7][12] = 1;
-    obst[7][15] = 1;
-    obst[7][18] = 1;
     obst[7][21] = 1;
+    obst[7][18] = 1;
+    obst[7][15] = 1;
+    obst[7][12] = 1;
+    obst[7][9] = 1;//invertido
+    obst[7][6] = 1;
+    obst[7][5] = 1;
+    obst[7][4] = 1;
 
 
-    obst[6][12] = 1;
+    obst[6][21] = 1;//invertido
     obst[6][18] = 1;
-    obst[6][21] = 1;
+    obst[6][12] = 1;
 
 
-    obst[5][12] = 1;
-    obst[5][18] = 1;
     obst[5][21] = 1;
+    obst[5][18] = 1;
+    obst[5][12] = 1;//invertido
 
 
-    obst[4][4] = 1;
-    obst[4][5] = 1;
     obst[4][6] = 1;
-    for (int row = 9; row <= 15; row++) {
-        obst[4][row] = 1;
+    obst[4][5] = 1;//invertido
+    obst[4][4] = 1;
+
+    for (int row = 15; row <= 9; row++) {
+        obst[4][row] = 1;                 //invertido
         if (row == 15) {
             break;
         }
     }
-    obst[4][18] = 1;
-    obst[4][21] = 1;
-    obst[4][22] = 1;
     obst[4][23] = 1;
+    obst[4][22] = 1;//invertido
+    obst[4][21] = 1;
+    obst[4][18] = 1;
 
 
     struct node * head = NULL;
